401d1f31c21021f68afb721d5b2885520c654628
csy <cotndus3@gmail.com>
2022-02-09 01:20:27 +0900

    Custom App

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import NavBar from "../components/NavBar";
 export default function Home(){
     return (
         <div>
-            <NavBar />
             <h1>Welcome :)</h1>
         </div>
     )
