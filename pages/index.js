3518d2f46cd32fad1751e38f28edbceaba42134a
csy <cotndus3@gmail.com>
2022-02-09 01:56:43 +0900

    Layout Pattern & Head

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import NavBar from "../components/NavBar";
+import Head from "next/head";
+import Seo from "../components/Seo";
 
 export default function Home(){
     return (
         <div>
+            <Seo title={"Home"} />
             <h1>Welcome :)</h1>
         </div>
     )
