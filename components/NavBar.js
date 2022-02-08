389b8fcba66b00f89403ebf9f482dbbfafcd94c0
csy <cotndus3@gmail.com>
2022-02-09 00:57:09 +0900

    style module.css #2

--- components/NavBar.js
+++ components/NavBar.js
@@ -8,12 +8,12 @@ export default function NavBar(){
     return(
         <nav>
             <Link href="/">
-                <a className={router.pathname === "/" ? styles.active : ""}>
+                <a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""} `}>
                     Home
                 </a>
             </Link>
             <Link href="/about">
-                <a className={router.pathname === "/about"? styles.active : ""}>
+                <a className={[styles.link, router.pathname === "/about"? styles.active : ""].join(" ")}>
                     About
                 </a>
             </Link>
