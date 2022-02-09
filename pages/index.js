8af53a17ec447e89a7422e245a73336a30d5b6eb
csy <cotndus3@gmail.com>
2022-02-09 23:37:14 +0900

    Movie Detail #2 another way navigating

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,25 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }){
+    const router = useRouter();
+    const movieClick = (id) =>{
+        router.push(`/movies/${id}`);
+    }
+
     return (
         <div className="container">
             <Seo title={"Home"} />
             {results?.map((movie) => (
-                <Link href={`/movies/${movie.id}`} key={movie.id} >
-                    <a>    
-                        <div className="movie" >
-                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-                            <h4>{movie.original_title}</h4>
-                        </div>
-                    </a>
-                </Link>
+                <div onClick={ () => movieClick(movie.id) }className="movie" key={movie.id} >
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                    <h4>
+                        <Link href={`/movies/${movie.id}`}>
+                            <a>{movie.original_title}   </a>
+                        </Link>
+                    </h4>
+                </div>
             ))}
             <style jsx>{`
                 .container {  
